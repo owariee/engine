17522fa54f38ab164f4a1761b3156f6537a99870
owarie <71178433+owarie@users.noreply.github.com>
2020-12-11 19:13:02 -0300

    Delete cmake_clean.cmake

--- debug/CMakeFiles/debug_lib.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/debug_lib.dir/Debug.cpp.o"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang CXX)
-  include(CMakeFiles/debug_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
