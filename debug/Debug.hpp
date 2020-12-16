0d70bdff76046ff47f5a8493d34517ad942341cd
wendellbrito <wendelrfbrito@gmail.com>
2020-12-16 15:18:16 -0300

    Escrito debug do projeto

--- debug/Debug.hpp
+++ debug/Debug.hpp
@@ -2,9 +2,12 @@
 
 #include <string>
 
-namespace Debug {
+namespace Debug 
+{
+    typedef enum { Error, Warning, Info, Trace } Flags;
+    typedef enum { Debug, Memory, Graphics, Vfs, Sound, Window, Gui, Lua } Subsystem;
 
-    void print(std::string message);
+    void print(int flag, int subsystem, std::string message);
 
 };
 
--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include "debug/Debug.hpp"
+#include "fmt/color.h"
+#include "fmt/core.h"
 
-int main(int argc, char* argv[]) {
-    Debug::print("Working");
+int main(int argc, char* argv[]) 
+{
+    Debug::print(6, Debug::Subsystem::Memory, "A memoria esta sendo estourada");
     return 0;
 }
 
